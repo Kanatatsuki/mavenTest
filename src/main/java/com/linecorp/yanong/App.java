ac29d6bcbcd6dd14fac038efbb5c883cb037b65f
Kanatatsuki <ttm.stoat.hp@gmail.com>
2017-08-16 10:06:25 +0000

    1st init

---
+++ src/main/java/com/linecorp/yanong/App.java
@@ -0,0 +1,13 @@
+package com.linecorp.yanong;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Util.printMessage("Hello, Util classes added here");
+    }
+}
---
+++ src/main/java/com/linecorp/yanong/Util.java
@@ -0,0 +1,7 @@
+package com.linecorp.yanong;
+
+public class Util{
+    public static void printMessage(String message){
+        System.out.println(message);
+    }
+}
